cb39248e017094c3f10f343c88963d42f20b33b2
Daniel Hast <hast.daniel@protonmail.com>
2025-06-21 12:14:05 -0400

    test: add more integration tests

--- integration-test/integration_tests.py
+++ integration-test/integration_tests.py
@@ -6,7 +6,9 @@ with passwordless run0.
 """
 
 import os
+import secrets
 import subprocess  # nosec
+import tempfile
 import unittest
 from typing import Final, Union
 
@@ -20,12 +22,19 @@ def cmd(name: str) -> str:
     return f"/usr/bin/{name}"
 
 
-def run0edit(*args: str, editor: str = EDITOR) -> None:
+def run0edit(*args: str, editor: str = EDITOR) -> subprocess.CompletedProcess[str]:
     """Call run0edit with the provided arguments"""
-    subprocess.run(
+    return subprocess.run(
         ["./run0edit-local", f"--editor={editor}", "--debug", "--no-prompt", "--", *args],
         check=True,
+        capture_output=True,
+        text=True,
     )  # nosec
+
+
+def random_filename(length: int = 32) -> str:
+    """Generates a random string that can be used as a filename (without creating the file)."""
+    return secrets.token_hex((length + 1) // 2)
 
 
 class TestFile:
@@ -37,10 +46,17 @@ class TestFile:
         *,
         root_owned: bool = True,
         immutable: bool = False,
+        create: bool = True,
+        is_dir: bool = False,
     ):
-        cmd_args = [cmd("mktemp")]
-        if directory is not None:
-            cmd_args += ["-p", directory]
+        if directory is None:
+            directory = tempfile.gettempdir()
+        if not create:
+            self._path = f"{directory}/{random_filename()}"
+            return
+        cmd_args = [cmd("mktemp"), "-p", directory]
+        if is_dir:
+            cmd_args.append("-d")
         if root_owned:
             cmd_args = [RUN0, *cmd_args]
         result = subprocess.run(
@@ -80,8 +96,8 @@ class TestFile:
 
     def __del__(self):
         """Remove the file."""
-        subprocess.run([RUN0, cmd("chattr"), "-i", "--", self._path], check=False)  # nosec
-        subprocess.run([RUN0, cmd("rm"), "--", self._path], check=False)  # nosec
+        subprocess.run([RUN0, cmd("chattr"), "-R", "-i", "--", self._path], check=False)  # nosec
+        subprocess.run([RUN0, cmd("rm"), "-rf", "--", self._path], check=False)  # nosec
 
 
 class TestIntegration(unittest.TestCase):
@@ -90,15 +106,62 @@ class TestIntegration(unittest.TestCase):
     def test_edit_file(self):
         """Should successfully change the contents of a root-owned file"""
         file = TestFile(directory="/root", root_owned=True)
-        run0edit(file.path)
+        result = run0edit(file.path)
+        self.assertEqual(result.stdout, "")
+        self.assertEqual(result.stderr, "")
         self.assertEqual(file.read().strip(), EDITED_TEXT)
+
+    def test_file_unchanged(self):
+        """Should note that unchanged file is unchanged"""
+        file = TestFile(directory="/var", root_owned=True)
+        result = run0edit(file.path, editor=cmd("true"))
+        self.assertEqual(result.stdout, f"run0edit: {file.path} unchanged\n")
+        self.assertEqual(result.stderr, "")
+        self.assertEqual(file.read().strip(), "")
+
+    def test_create_file(self):
+        """Should successfully create a file in a root-owned directory"""
+        file = TestFile(directory="/var", create=False)
+        result = run0edit(file.path)
+        self.assertEqual(result.stdout, "")
+        self.assertEqual(result.stderr, "")
+        self.assertEqual(file.read().strip(), EDITED_TEXT)
+
+    def test_not_created(self):
+        """Should not create empty file"""
+        file = TestFile(directory="/var", create=False)
+        result = run0edit(file.path, editor=cmd("true"))
+        self.assertEqual(result.stdout, f"run0edit: {file.path} not created\n")
+        self.assertEqual(result.stderr, "")
+        self.assertFalse(os.path.exists(file.path))
 
     def test_edit_immutable_file(self):
         """Should successfully change the contents of immutable file and restore the attribute"""
         file = TestFile(root_owned=False, immutable=True)
-        run0edit(file.path)
+        result = run0edit(file.path)
+        self.assertEqual(result.stdout.strip(), "Immutable attribute reapplied.")
+        self.assertEqual(result.stderr, "")
         self.assertEqual(file.read().strip(), EDITED_TEXT)
         self.assertTrue(file.is_immutable())
+
+    def test_immutable_file_unchanged(self):
+        """Should not touch immutable flag on unchanged immutable file"""
+        file = TestFile(root_owned=False, immutable=True)
+        result = run0edit(file.path, editor=cmd("true"))
+        self.assertEqual(result.stdout, f"run0edit: {file.path} unchanged\n")
+        self.assertEqual(result.stderr, "")
+        self.assertEqual(file.read().strip(), "")
+
+    def test_create_file_in_immutable_directory(self):
+        """Should successfully create new file in immutable directory"""
+        directory = TestFile(root_owned=False, immutable=True, is_dir=True)
+        file = TestFile(directory=directory.path, create=False)
+        result = run0edit(file.path)
+        self.assertEqual(result.stdout.strip(), "Immutable attribute reapplied.")
+        self.assertEqual(result.stderr, "")
+        self.assertEqual(file.read().strip(), EDITED_TEXT)
+        self.assertTrue(directory.is_immutable())
+        self.assertFalse(file.is_immutable())
 
 
 if __name__ == "__main__":
